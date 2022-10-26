5b8dd78260999e0f29c9b6ad0dbd893a8e5a5e73
w91379137 <w913791379137@gmail.com>
2022-10-26 15:11:07 +0800

    [feat] 新增拖曳模組 DragDropModule

--- yc-anguler-share-material.module.ts
+++ yc-anguler-share-material.module.ts
@@ -1,35 +1,37 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+// 幫助匯入大量的 material 套件
+// https://ithelp.ithome.com.tw/articles/10192517
+//
+// components
+// https://material.angular.io/components/categories
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-// 幫助匯入大量的 material 套件
-// https://ithelp.ithome.com.tw/articles/10192517
+// cdk
+// https://material.angular.io/cdk/categories
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
+const ImportExports = [
+  CommonModule,
+  FormsModule,
+  // components
+  MatSliderModule,
+  MatSelectModule,
+  MatDialogModule,
+  MatSlideToggleModule,
+  MatButtonToggleModule,
+  MatCheckboxModule,
+  // cdk
+  DragDropModule,
+]
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    FormsModule,
-    MatSliderModule,
-    MatSelectModule,
-    MatDialogModule,
-    MatSlideToggleModule,
-    MatButtonToggleModule,
-    MatCheckboxModule,
-  ],
-  exports: [
-    FormsModule,
-    MatSliderModule,
-    MatSelectModule,
-    MatDialogModule,
-    MatSlideToggleModule,
-    MatButtonToggleModule,
-    MatCheckboxModule,
-  ],
+  imports: ImportExports,
+  exports: ImportExports,
 })
 export class YcAngulerShareMaterialModule { }
